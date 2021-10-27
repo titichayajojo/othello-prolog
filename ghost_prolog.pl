416d99e3566f435c7fa5d69b0fd07d988589692c
Ojo <titichaya_love@hotmail.com>
2021-10-27 17:28:37 +0700

    [ADD] facts in ghost.pl
    adding 24 facts in ghost.pl

--- ghost_prolog.pl
+++ ghost_prolog.pl
@@ -24,3 +24,31 @@ edge(node(21,5), node(26,5), 5, east).
 edge(node(21,5), node(21,8), 3, south).
 edge(node(21,8), node(26,8), 5, east).
 edge(node(26,5), node(26,8), 3, south).
+edge(node(1,20),node(6,20),5,east).
+edge(node(1,20),node(3,26),8,south).
+edge(node(6,20),node(6,23),3,south).
+edge(node(6,23),node(6,26),3,south).
+edge(node(6,26),node(3,26),3,west).
+edge(node(3,26),node(1,26),2,west).
+edge(node(1,26),node(1,29),3,south).
+edge(node(1,29),node(12,29),11,east).
+edge(node(6,23),node(9,23),3,east).
+edge(node(9,23),node(12,23),3,east).
+edge(node(12,23),node(15,23),3,east).
+edge(node(15,23),node(18,23),3,east).
+edge(node(18,23),node(21,23),3,east).
+edge(node(9,23),node(12,29),9,south).
+edge(node(12,29),node(15,29),3,east).
+edge(node(18,23),node(15,29),9,south).
+edge(node(21,20),node(21,23),5,south).
+edge(node(21,20),node(26,20),5,east).
+edge(node(21,23),node(21,26),3,south).
+edge(node(21,26),node(24,26),3,east).
+edge(node(21,26),node(24,2),5,east).
+edge(node(1,20),node(6,20),5,east).
+edge(node(1,20),node(6,20),5,east).
+edge(node(1,20),node(6,20),5,east).
+
+
+
+
